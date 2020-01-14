b82dd482a8580fc87b3c91e6bce0150473761fe0
Vinicius Rocha <viniciusr.rocha@outlook.com>
2020-01-14 10:53:46 -0300

    Entendendo API REST FUL

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,18 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
+
+// Métodos HTTP: GET, POST, PUT, DELELTE
+
+// Tipos de parâmetros:
+
+// Query Params: request.query (Filtros, ordenação, paginação, ...)
+//  Route Params: request.params (Identificador um recurso na alteração ou remoçãao)
+// Body: request.body (Dados para criação ou alteração de um registro)
+
+app.post('/users/:id', (request, response) => {
+    console.log(request.body);
     return response.json({ message: 'Hello OmniStack' });
 });
 
